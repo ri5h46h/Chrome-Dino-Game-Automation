0f934711592a93fb6893e4998191dfaa48c4a5b5
Rishabh Narayan <62702796+ri5h46h@users.noreply.github.com>
2020-05-09 12:16:25 +0530

    test.py for main added

---
+++ test.py
@@ -0,0 +1,47 @@
+import pyautogui # pip install pyautogui
+from PIL import Image, ImageGrab # pip install pillow
+# from numpy import asarray
+import time
+
+def hit(key):
+    pyautogui.keyDown(key)
+    return
+
+def isCollide(data):
+    # Draw the rectangle for birds
+    for i in range(300, 415):
+        for j in range(500, 540):
+            if data[i, j] < 100:
+                hit("up")
+                return
+
+    '''for i in range(300, 415):
+        for j in range(500, 540):
+            if data[i, j] < 100:
+                hit("up")
+                return'''
+    return
+
+if __name__ == "__main__":
+    print("Hey.. Dino game about to start in 3 seconds")
+    time.sleep(2)
+    # hit('up') 
+
+    #while True:
+    image = ImageGrab.grab().convert('L')  
+    data = image.load()
+        
+            
+        # print(asarray(image))
+    
+        # Draw the rectangle for cactus
+    for i in range(250, 365):
+        for j in range(470, 510):
+            data[i, j] = 0
+        
+        # Draw the rectangle for birds
+    '''for i in range(200, 250):
+        for j in range(280, 430):
+            data[i, j] = 171'''
+
+    image.show()
